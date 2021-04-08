f1704621b150e4615617c931179460c5633ee120
Mohammed Mizban <Mohammed.mizban97@gmail.com>
2021-04-08 17:01:28 +0300

    delete AI class

--- src/AI.kt
+++
@@ -1,6 +0,0 @@
-class AI { //NOT YET USED
-    //easy - will make losing moves
-    //medium - will try to make a tictactoe
-    //hard - will block other player's tictactoe
-    //stupid - will make random moves (easiest to code)
-}
