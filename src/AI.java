14487a7643eb5c5853db51fc34587f96df2f2553
Mohammed Mizban <Mohammed.mizban97@gmail.com>
2021-04-08 16:14:03 +0300

    convert code from java to kotlin

--- src/AI.java
+++
@@ -1,7 +0,0 @@
-public class AI {
-//NOT YET USED
-//easy - will make losing moves
-//medium - will try to make a tictactoe
-//hard - will block other player's tictactoe
-//stupid - will make random moves (easiest to code)
-}
